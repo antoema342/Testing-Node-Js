cbde41849fad7a28dc6133886034a877ff49bbf8
Anto <35158251+antoema342@users.noreply.github.com>
2019-07-25 23:20:52 +0700

    initial commit

--- routes/users.js
+++ routes/users.js
@@ -1,44 +1,85 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+var mysql = require('mysql');
+
 
 //user model
-const User = require('../models/User');
+// const User = require('../models/User');
 
 //login page 
-router.get('/login',(req, res)=>res.render('login'));
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "sampledb"
+});
+//LOGIN PAGE
+router.get('/login', (req, res) => res.render('login'));
+
+//PROSES LOGIN
+router.post('/login', (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  let sql = "SELECT * FROM user WHERE email = '" + email + "' AND password = '" + password + "'";
+  console.log(sql)
+  con.query(sql, function (err, result, fields) {
+    if (err) throw err;
+    if (result.length > 0) {
+      console.log("ada data")
+      // KALAU LOGIN BERHASIL SILAHKAN REDIRECT KE HALAMAN SELANJUTNYA DISINI
+    }
+    else {
+      //KALAU GAGAL LOGIN DISINI
+    }
+    console.log(result);
+  })
+})
 
 //register page 
-router.get('/register',(req, res)=>res.render('register'));
+router.get('/register', (req, res) => res.render('register'));
 
 // Register
 router.post('/register', (req, res) => {
   const { name, email, password, password2 } = req.body;
   let errors = [];
-  
+
   //check 
-  if(!name || !email || !password || !password2){
-      errors.push({msg: 'Please fill all fields'});
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: 'Please fill all fields' });
   }
 
   //check valid password 1 and 2 
-  if(password !== password2){
-      errors.push({msg: 'password nya engga cocok ea babang !'});
+  if (password !== password2) {
+    errors.push({ msg: 'password nya engga cocok ea babang !' });
   }
   if (password.length > 11) {
     errors.push({ msg: 'Password harus 11 :) ' });
   }
 
-  if(errors.length >0){
-    res.render('register',{
-        errors,
-        name,
-        email,
-        password,
-        password2
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      password2
     });
-      }
-      
+  }
+  let sql = "INSERT INTO user (username,email,password,password2) VALUES ('"+name+"','"+email+"','"+password+"','"+password2+"')";
+  console.log(sql)
+  con.query(sql, function (err, result, fields) {
+    if (err) throw err;
+    if (result != null) {
+      console.log("ada data")
+      // KALAU REGISTRASI BERHASIL SILAHKAN REDIRECT KE HALAMAN SELANJUTNYA DISINI
+    }
+    else {
+      //KALAU GAGAL REGISTRASI DISINI
+    }
+    console.log(result);
+  })
+
 });
 
 module.exports = router;
---
+++ sampledb (1).sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 24, 2019 at 01:12 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.1.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sampledb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(11) NOT NULL,
+  `password2` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `password2`) VALUES
+(1, 'testing', '', 'testing123', ''),
+(2, 'Test', 'admin@gmail.com', '123', '123'),
+(3, 'sd', 'sadas@gmail.com', 'sda', 'das'),
+(4, 'sd', 'sadas@gmail.com', 'sda', 'das'),
+(5, 'sdhjsagdj', 'sadas@gmail.com', 'sda', 'das');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
